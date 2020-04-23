fc4a7e94531e7bfaa388ea5c5737a89b5b7a07a2
marslanali <arslanali800@hotmail.com>
2020-04-23 23:54:50 +0500

    adding opencv node

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,7 @@
 # new environment variables
 
 # modified environment variables
+export LD_LIBRARY_PATH="/home/arslanali/turtlebot3/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
+export PKG_CONFIG_PATH="/home/arslanali/turtlebot3/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/arslanali/quadrotor-simulation/build"
-export PYTHONPATH="/home/arslanali/quadrotor-simulation/devel/lib/python2.7/dist-packages:/home/arslanali/turtlebot3/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:/usr/lib/python2.7/dist-packages"
+export PYTHONPATH="/home/arslanali/turtlebot3/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:/usr/lib/python2.7/dist-packages"
